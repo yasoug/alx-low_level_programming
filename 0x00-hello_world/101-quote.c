8916c3cd60068331d39252dd96d3ca4c6cad50ef
yasoug <ougouisyassine@gmail.com>
2023-03-16 14:57:46 -0700

    understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - entry point
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful");
-	fprintf(stderr, "\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 70);
 	return (1);
 }
