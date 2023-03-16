39a2dc1e2ef75417b2e1cadc6462386c9f465fe8
yasoug <ougouisyassine@gmail.com>
2023-03-16 14:32:24 -0700

    understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 1 Always (succes)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
