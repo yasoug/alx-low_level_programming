e118041e6437d6a995a4d1fd06068c92c0c0f965
yasoug <ougouisyassine@gmail.com>
2023-03-29 13:33:58 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @s: input string
+ * Return: pointer to the resulting string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
