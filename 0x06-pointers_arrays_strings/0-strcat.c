9c18feeea129956771f8b00691d422562295d0ef
yasoug <ougouisyassine@gmail.com>
2023-03-29 10:54:54 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,7 +15,7 @@ char *_strcat(char *dest, char *src)
 	while (dest[l])
 		l++;
 
-	for (i = 0; src[i] = '\0'; i++)
+	for (i = 0; src[i] == '\0'; i++)
 		dest[l + i] = src[i];
 
 	dest[l + i] = '\0';
