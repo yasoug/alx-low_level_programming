2b1f8e0c15e90ee5361303aafc0771715e3dee20
yasoug <ougouisyassine@gmail.com>
2023-04-05 11:29:31 -0700

    it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns x raised to the power of y
+ * @x: the base
+ * @y: the exponent
+ * Return: x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
