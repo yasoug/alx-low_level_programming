680c780136064ba0717427169d77b2ca642eefac
yasoug <ougouisyassine@gmail.com>
2023-04-09 08:23:46 -0700

    It ain't what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints the name of the program
+ * @argc: argument counter
+ * @argv: array of arguments
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
