ce26438998d11268bebce31c0f8e6054d05c33e9
yasoug <ougouisyassine@gmail.com>
2023-04-11 12:51:02 -0700

    It isn't the mountains ahead to climb that wear you out

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ * Return: length of the string s
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * argstostr - function that concatenates all the arguments of program
+ * @ac: the number of arguments
+ * @av: the arguments
+ * Return: pointer to a new string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int len = 0;
+	int i;
+	int j;
+	int k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		len += _strlen(av[i]);
+	len += (ac + 1);
+
+	s = malloc(len * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++, k++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++, k++)
+			s[k] = av[i][j];
+		s[k] = '\n';
+	}
+	return (s);
+}
