bbf2f3f1dab1696a5d4db14487eca5d93fd9f480
yasoug <ougouisyassine@gmail.com>
2023-04-13 11:22:04 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,10 +41,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
-	if (n > len2)
+	if ((int)n > len2)
 		n = len2;
-	if (n < 0)
-		return (NULL);
+	
 	concat = malloc((len1 + n + 1) * sizeof(char));
 	if (concat == NULL)
 		return (NULL);
@@ -52,7 +51,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
 
-	for (j = 0; j < n; j++)
+	for (j = 0; j < (int)n; j++)
 		concat[j + i] = s2[j];
 
 	concat[j + i] = '\0';
