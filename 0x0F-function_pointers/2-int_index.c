c49996d118442a4f62ed55dad47ec450300fd906
yasoug <ougouisyassine@gmail.com>
2023-04-19 11:33:46 -0700

    task 2 commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ * @array: input array
+ * @size: size of the array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: the index of the first match, -1 if no match
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+
+	return (-1);
+}
