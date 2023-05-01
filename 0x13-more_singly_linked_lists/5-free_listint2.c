30dd16c2d03abc0947b043858c3017c3469810bd
yasoug <ougouisyassine@gmail.com>
2023-05-01 07:06:36 -0700

    task 5 : first commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list, and sets head to NULL
+ * @head: linked list to be freed
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
+	}
+}
