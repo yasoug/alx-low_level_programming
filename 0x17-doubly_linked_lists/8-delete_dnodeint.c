15655fdd8eb9a9e2f4d1cc047bed47d6ac31b1fe
yasoug <ougouisyassine@gmail.com>
2023-06-15 13:17:37 -0700

    task 8 : commit 2 (correct variable)

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -25,7 +25,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (index && del->next)
+	while (index && tmp->next)
 	{
 		index--;
 		tmp = tmp->next;
