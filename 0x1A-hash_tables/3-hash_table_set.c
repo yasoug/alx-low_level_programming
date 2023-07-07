602d14ba3e8a093b0e5b3363a1ce4e9f817604b9
yasoug <ougouisyassine@gmail.com>
2023-07-07 05:36:33 -0700

    task 3 : commit 1

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: the hash table to add or update the key/value to
+ * @key: key
+ * @value: the value associated with the key
+ * Return: 1 if it succeeded, or 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i;
+	hash_node_t *new_node, *temp;
+	char *new_value;
+
+	if (key == NULL || ht == NULL || value == NULL)
+		return (0);
+
+	i = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[i];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (!new_value)
+				return (0);
+			free(temp->value);
+			temp->value = new_value;
+			return (1);
+		}
+		temp = temp->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[i];
+	ht->array[i] = new_node;
+	return (1);
+}
