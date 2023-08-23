aab53d703f7afd3e6c885ad6182fa718fd6181c8
yasoug <ougouisyassine@gmail.com>
2023-08-23 06:50:34 -0700

    task 5 : commit 1

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Island Perimeter Module
+"""
+
+
+def island_perimeter(grid):
+    """
+    Return the perimeter of the island described in grid
+    """
+    height = len(grid)
+    width = len(grid[0])
+    perimeter = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == (height - 1) or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == (width - 1) or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
