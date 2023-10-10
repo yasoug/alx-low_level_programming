342961cf23c7e0649fc8271767abb5e607740aca
yasoug <ougouisyassine@gmail.com>
2023-10-10 13:47:20 -0700

    task 9 : commit 2 (fix betty style)

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -20,7 +20,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	while (low <= high)
 	{
-		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+				(value - array[low]));
 		if (pos < low || pos > high)
 		{
 			printf("Value checked array[%ld] is out of range\n", pos);
